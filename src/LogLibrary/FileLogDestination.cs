aa9332ce8c05f0e52961d8935bb052360e998b0f
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-08-19 17:46:57 +0200

    Added file destination with warning

---
+++ src/LogLibrary/FileLogDestination.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace LogLibrary
+{
+    public class FileLogDestination : ILogDestination
+    {
+        private FileStream _fileStream;
+        private StreamWriter _streamWriter;
+
+        public FileLogDestination(String fileName)
+        {
+            _fileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+            _fileStream.Seek(0, SeekOrigin.End);
+            _streamWriter = new StreamWriter(_fileStream);
+        }
+        public void AddLog(LogMessage message)
+        {
+            _streamWriter.Write($"{message.Level}[{message.Timestamp}] - {message.RenderedMessage}");
+        }
+    }
+}
